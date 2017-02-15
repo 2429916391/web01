5dd30942b4e2db71c0c1f8ecff040a9186db6345
webC2017 <you@example.com>
2017-02-15 19:00:24 +0800

    Initial commit

---
+++ src/main/java/com/fz/Hello.java
@@ -0,0 +1,10 @@
+package com.fz;
+
+/**
+ * Created by 2429916391@qq.com on 2017/2/15 18:59.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
